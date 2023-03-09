a5940736cdaf8bc64504a0e0a28043593fa6ae8c
spirus-dev <43066360+spirus-dev@users.noreply.github.com>
2023-03-09 10:00:41 +0530

    Completed all the tasks mentioned in assignment
    Calculator is finished with all the necessary things that has been mentioned in the assignment. Some minor bugs still needs to be addressed that we'll look afterwards. Currently putting this in backlog.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let firstNumber=0;
+let secondNumber=0;
+let opeartor="";
+let isDecimalPressed=false;
 const input=document.querySelector(".input p");
 const output=document.querySelector(".output p");
 const buttons=document.querySelectorAll('.button');
@@ -8,11 +12,55 @@ buttons.forEach(
 function buttonClick(e){
     const buttonText=e.srcElement.innerText;
     if(!["AC","DEL","="].includes(buttonText)){
-        input.innerText+=buttonText;
+        if(["+","-","*","/"].includes(buttonText)){  
+            if(firstNumber==""){
+                firstNumber=input.innerText;
+                console.log(firstNumber);
+            }
+            else if(secondNumber==""){
+                output.innerText="";
+                secondNumber=input.innerText.slice(input.innerText.lastIndexOf(opeartor)+1);
+                console.log(secondNumber);
+                try {
+                        let answer=opearte(opeartor,Number(firstNumber),Number(secondNumber));
+                        if(answer=="Can't divide by zero"){
+                            input.innerText="";
+                            output.innerText=answer;
+                            firstNumber="0";
+                            secondNumber="";
+                        }
+                        else{
+                            input.innerText=Math.round(answer*100)/100;
+                            firstNumber=answer;
+                            secondNumber="";
+                        }          
+                } 
+                catch (error) {
+                    console.log(error)
+                    output.innerText="INVALID EXPRESSION"; 
+                } 
+            }
+            opeartor=buttonText;
+            isDecimalPressed=false;
+            input.innerText+=buttonText;
+        }
+        else if(buttonText=="."){
+            if(!isDecimalPressed){
+                input.innerText+=buttonText;
+                isDecimalPressed=true;
+            }
+        }
+        else{
+            input.innerText+=buttonText;
+        }
     }
     else if(buttonText=="AC"){
         input.innerText="";
         output.innerText="";
+        firstNumber=0;
+        secondNumber=0;
+        opeartor="";
+        isDecimalPressed=false;
     }
     else if(buttonText=="DEL"){
         input.innerText=input.innerText.slice(0,-1);
@@ -20,12 +68,25 @@ function buttonClick(e){
     else{
         output.innerText="";
         try {
-            const answer=eval(input.innerText);
-            input.innerText="";
-            output.innerText=Math.round(answer*100)/100;            
+            if(opeartor!=""){
+                secondNumber=input.innerText.slice(input.innerText.lastIndexOf(opeartor)+1);
+                input.innerText="";
+                let answer=opearte(opeartor,Number(firstNumber),Number(secondNumber));
+                firstNumber="";
+                secondNumber="";
+                opeartor="";
+                if(answer=="Can't divide by zero"){
+                    output.innerHTML=answer;
+                }
+                else{
+                    output.innerText=Math.round(answer*100)/100;
+                }   
+            }           
         } catch (error) {
+            console.log(error)
             output.innerText="INVALID EXPRESSION"; 
         }
+        isDecimalPressed=false;
     }
 }
 
@@ -42,5 +103,29 @@ function multiply(firstNumber,secondNumber){
 }
 
 function divide(firstNumber,secondNumber){
-    return firstNumber/secondNumber;
+    if(secondNumber==0){
+        return "Can't divide by zero";
+    }
+    else{
+        return firstNumber/secondNumber;
+    }
+}
+
+function opearte(opeartor,firstNumber,secondNumber){
+    switch (opeartor) {
+        case '+':
+            return add(firstNumber,secondNumber)    
+            break;
+        case '-':
+            return subtract(firstNumber,secondNumber)
+            break;
+        case '*':
+            return multiply(firstNumber,secondNumber)
+            break;
+        case '/':
+            return divide(firstNumber,secondNumber)
+            break;
+        default:
+            break;
+    }
 }
